51905b21e69cfd66a609798fe64a695164c5c2ae
SecretSlater <christopher.slater@mavs.uta.edu>
2017-02-25 07:09:50 -0600

    Updated DefaultEngine.ini for VR and added VRSimpleCharacter class (mostly to see if the plugin would compile)

---
+++ Source/WelcomeHome/Public/MainCharacter.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "SimpleChar/VRSimpleCharacter.h"
+#include "MainCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WELCOMEHOME_API AMainCharacter : public AVRSimpleCharacter
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
