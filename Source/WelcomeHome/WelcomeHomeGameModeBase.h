9944a119870f7e20eec6f8a67d5791ee5d0e4333
SecretSlater <christopher.slater@mavs.uta.edu>
2017-02-25 06:23:15 -0600

    Added Project files

---
+++ Source/WelcomeHome/WelcomeHomeGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "WelcomeHomeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WELCOMEHOME_API AWelcomeHomeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
